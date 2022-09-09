a40aa27683ca3612f93062f014c1c4f0a9d14f3c
gustav042 <gustavofelipereira42@gmail.com>
2022-09-09 05:01:28 -0300

    first commit

---
+++ b.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+plt.rcParams['figure.figsize'] = [8, 8]
+plt.rcParams.update({'font.size': 18})
+
+# Load dataset
+H = np.loadtxt(os.path.join('..','..','DATA','multire'))
+b = H[26:159,0]
+A = H[26:159,1:5]
+
+# Solve Ax=b using SVD
+U, S, VT = np.linalg.svd(A,full_matrices=0)
+x = VT.T @ np.linalg.inv(np.diag(S)) @ U.T @ b
+
+plt.plot(b, Color='k', LineWidth=2, label='Heat Data') # True relationship
+plt.plot(A@x, '-o', Color='r', LineWidth=1.5, MarkerSize=6, label='Regression')
+plt.legend()
+plt.show()
